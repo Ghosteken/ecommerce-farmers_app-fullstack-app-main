f964cfcfb11c72c411f6fb83ca41b9ffeaa47918
nicholas <aigberuan6@gmail.com>
2025-03-31 21:04:59 +0100

    config

--- ecommerce-nextjs-fullstack-app-main/next.config.ts
+++ ecommerce-nextjs-fullstack-app-main/next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     domains: ["files.stripe.com"],
+  },
+  env: {
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
   },
 };
 
